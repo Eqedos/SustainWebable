431d8a8c1dd4083554158d96ba8cddc52ea173c2
Eqedos <parthkumarj117@gmail.com>
2023-05-02 02:19:45 +0800

    init

---
+++ app/src/main/java/com/example/sustainwebable/LoginRegPage.java
@@ -0,0 +1,14 @@
+package com.example.sustainwebable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginRegPage extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_reg_page);
+    }
+}
