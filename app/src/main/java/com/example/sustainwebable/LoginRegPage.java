1e50144828e01186e4c0304964ffe20ffe4c5502
Eqedos <parthkumarj117@gmail.com>
2023-05-02 02:47:22 +0800

    Working in login/registration

--- app/src/main/java/com/example/sustainwebable/LoginRegPage.java
+++ app/src/main/java/com/example/sustainwebable/LoginRegPage.java
@@ -1,14 +1,34 @@
 package com.example.sustainwebable;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginRegPage extends BaseActivity {
+    private Button mRegister,mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_reg_page);
+        mRegister = (Button) findViewById(R.id.register);
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginRegPage.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginRegPage.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
